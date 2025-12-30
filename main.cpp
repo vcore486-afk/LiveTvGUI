54161dc562d6ab3ca93abcf14dd49a1a107edaed
Your Name <you@example.com>
2025-12-30 12:32:00 +0300

    запуск приложения по центру экрана 	изменено:      main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "pythonmanager.h"  // ← обязательно!
 #include <QApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
@@ -18,8 +19,16 @@ int main(int argc, char *argv[])
   // Инициализируем Python ОДИН РАЗ
     PythonManager::instance();
     MainWindow w;
+    QScreen* screen = QGuiApplication::primaryScreen();
+    if (!screen) {
+        qWarning("No primary screen found");
+        return EXIT_FAILURE;
+    }
+    const QRect availableGeometry = screen->availableGeometry();
+    const QPoint centerPosition = availableGeometry.center() -
+    QPoint(w.width() / 2, w.height() / 2);
+    w.move(centerPosition);  // перемещаем окно в центр экрана
     w.callPythonScript(":/config.txt");
-    w.callPythonScript(":/proxy.txt");
     w.show();
     return a.exec();
 }
